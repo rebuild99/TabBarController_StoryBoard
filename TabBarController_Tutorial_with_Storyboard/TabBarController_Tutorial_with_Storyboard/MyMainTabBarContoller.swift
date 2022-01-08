73e5cb17bf5accc3fb91ed6240ff74b43cb78729
rebuild99 <97108037+rebuild99@users.noreply.github.com>
2022-01-08 23:35:04 +0900

    Add files via upload

---
+++ TabBarController_Tutorial_with_Storyboard/TabBarController_Tutorial_with_Storyboard/MyMainTabBarContoller.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TabBarController_Tutorial_with_Storyboard
+//
+//  Created by Test on 2022/01/08.
+//
+
+import UIKit
+
+class MyMainTabBarContoller: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
